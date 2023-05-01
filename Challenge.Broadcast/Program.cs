5e918926fdb7e5154354e899bf34033138c4ce1b
Nikolaus Spring <nikolaus.spring@gmail.com>
2023-05-01 12:14:21 +0200

    Implement fault-tolerant Multi-Node Broadcast

--- Challenge.Broadcast/Program.cs
+++ Challenge.Broadcast/Program.cs
@@ -7,16 +7,14 @@ var messages = new ConcurrentDictionary<int, byte>();
 
 var node = new Node();
 
-void HandleBroadcast(Message message)
+async Task HandleBroadcast(Message message)
 {
     var body = message.GetBody<BroadcastBody>();
     var messageValue = body.Message;
     node.Log("Received message " + messageValue);
 
-    if (message.Source.StartsWith("c"))
-    {
-        node.Reply(message, new Body { Type = "broadcast_ok" });
-    }
+    node.Reply(message, new Body { Type = "broadcast_ok" });
+    node.Log("Replied to message " + messageValue + " with broadcast_ok");
 
     if (!messages.TryAdd(body.Message, 0x0))
     {
@@ -24,26 +22,45 @@ void HandleBroadcast(Message message)
         return;
     }
 
-    foreach (var neighbor in neighbors.Where(neighbor => !neighbor.Equals(message.Source)))
+    var unacked = new ConcurrentDictionary<string, byte>(neighbors.Select(x => new KeyValuePair<string, byte>(x, 0x0)));
+    unacked.TryRemove(message.Source, out _);
+    node.Log("Broadcasting message " + messageValue + " to " + string.Join(",", unacked));
+    while (!unacked.IsEmpty) 
     {
-        node.Rpc(neighbor, new BroadcastBody { Type = "broadcast", Message = body.Message },
-            msg => node.Log($"{neighbor} replied {msg.Body["type"]} for {body.Message}"));
+        foreach (var neighbor in unacked.Keys)
+        {
+            node.Rpc(neighbor, new BroadcastBody { Type = "broadcast", Message = body.Message },
+            msg =>
+            {
+                node.Log("Message " + messageValue + " acked by " + neighbor + ", removing from unacked");
+                unacked.TryRemove(neighbor, out _);
+                return Task.CompletedTask;
+            });
+        }
+        await Task.Delay(1000);
+        if (!unacked.IsEmpty)
+        {
+            node.Log("Message " + messageValue + " not acked by " + string.Join(",", unacked) + ", retrying");
+        }
     }
+    node.Log("Message " + messageValue + " broadcasted to all neighbors");
 }
 
-void HandleTopology(Message message)
+Task HandleTopology(Message message)
 {
     var body = message.GetBody<TopologyBody>();
     neighbors = body.Topology[node.Id];
     node.Log("Neighbors: [" + string.Join(",", neighbors) + "]");
     node.Reply(message, new Body { Type = "topology_ok" });
+    return Task.CompletedTask;
 }
 
-void HandleRead(Message message)
+Task HandleRead(Message message)
 {
     var response = new ReadResponseBody { Type = "read_ok", Messages = messages.Keys.ToList() };
     node.Log("read [" + string.Join(",", response.Messages) + "]");
     node.Reply(message, response);
+    return Task.CompletedTask;
 }
 
 node.On("broadcast", HandleBroadcast)
